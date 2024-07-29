1748ecc0fca480ca51027b9e832728e15c995f78
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2024-07-30 01:32:57 +0300

    update the Home Fragment to link it with Recycler view

--- app/src/main/java/com/example/recipeapp/Recipe/Home/HomeFragment.kt
+++ app/src/main/java/com/example/recipeapp/Recipe/Home/HomeFragment.kt
@@ -5,17 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.recipeapp.R
 
 class HomeFragment : Fragment() {
+
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        viewModel.getRecipesByLetter()
+        viewModel.recipes.observe(viewLifecycleOwner){recipeResponce ->
+            val adapter = listRecipeAdapter(recipeResponce)
+            val recyclerView = view?.findViewById<RecyclerView>(R.id.rv_popular_recipe)
+            recyclerView?.adapter = adapter
+            recyclerView?.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.HORIZONTAL, false)
+        }
+
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
 
 
 }
