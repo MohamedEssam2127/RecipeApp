efbdc599b766bb05ed03431b890311c158a38273
Youssef-Mustafa <youssef2.mostafa.1@gmail.com>
2024-07-30 01:21:32 +0300

    finish Search of meal and edit RecipeRepository to print log of details

--- app/src/main/java/com/example/recipeapp/network/RecipeRepository.kt
+++ app/src/main/java/com/example/recipeapp/network/RecipeRepository.kt
@@ -1,5 +1,6 @@
 package com.example.recipeapp.network
 
+import android.util.Log
 import com.example.recipeapp.models.CategoriesResponse
 import com.example.recipeapp.models.Meal
 import com.example.recipeapp.models.RecipeResponse
@@ -8,23 +9,42 @@ class RecipeRepository {
 
     private val api = RetrofitInstance.api
 
+    companion object {
+        private const val TAG = "RecipeRepository"
+    }
+
     suspend fun getRandomMeal(): Meal {
-        return api.getRandomMeal()
+        Log.d(TAG, "Requesting random meal")
+        return api.getRandomMeal().also {
+            Log.d(TAG, "Received random meal: $it")
+        }
     }
 
     suspend fun getMealsByFirstLetter(letter: String): RecipeResponse {
-        return api.getMealsByFirstLetter(letter)
+        Log.d(TAG, "Requesting meals by first letter: $letter")
+        return api.getMealsByFirstLetter(letter).also {
+            Log.d(TAG, "Received meals by first letter: $it")
+        }
     }
 
     suspend fun searchMealByName(phrase: String): RecipeResponse {
-        return api.searchMealByName(phrase)
+        Log.d(TAG, "Searching meals by name: $phrase")
+        return api.searchMealByName(phrase).also {
+            Log.d(TAG, "Received search results: $it")
+        }
     }
 
     suspend fun listAllMealCategories(): CategoriesResponse {
-        return api.listAllMealCategouris()
+        Log.d(TAG, "Requesting all meal categories")
+        return api.listAllMealCategouris().also {
+            Log.d(TAG, "Received meal categories: $it")
+        }
     }
 
     suspend fun getMealsByCategory(category: String): RecipeResponse {
-        return api.getMealsByCategory(category)
+        Log.d(TAG, "Requesting meals by category: $category")
+        return api.getMealsByCategory(category).also {
+            Log.d(TAG, "Received meals by category: $it")
+        }
     }
 }
