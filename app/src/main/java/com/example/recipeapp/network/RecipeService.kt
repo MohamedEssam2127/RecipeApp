790d6c0eb1b557c775b69aec9920e1ef426bf6e1
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2024-07-29 18:28:54 +0300

    edit the Get links (trmove part of the base exist)

--- app/src/main/java/com/example/recipeapp/network/RecipeService.kt
+++ app/src/main/java/com/example/recipeapp/network/RecipeService.kt
@@ -8,23 +8,23 @@ import retrofit2.http.Query
 
 interface RecipeService {
 
-    @GET("api/json/v1/1/random.php")
+    @GET("random.php")
     suspend fun getRandomMeal(): Meal
 
-    @GET("api/json/v1/1/search.php")
+    @GET("search.php")
     suspend fun getMealsByFirstLetter(
         @Query("f") letter: String
     ): RecipeResponse
 
-    @GET("api/json/v1/1/search.php")
+    @GET("search.php")
     suspend fun searchMealByName(
         @Query("s") phrase: String
     ): RecipeResponse
 
-    @GET("api/json/v1/1/categories.php")
+    @GET("categories.php")
     suspend fun listAllMealCategouris(): CategoriesResponse
 
-    @GET("api/json/v1/1/filter.php")
+    @GET("filter.php")
     suspend fun getMealsByCategory(
         @Query("c") category: String
     )
