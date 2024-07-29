be75e5334d5f6f7d15054301853a441f791c515c
Omar Adel Hussein Badawy <102391009+OmarAdelBadawy0@users.noreply.github.com>
2024-07-29 17:29:43 +0300

    add the services will be used from the API in RecipeService

--- app/src/main/java/com/example/recipeapp/network/RecipeService.kt
+++ app/src/main/java/com/example/recipeapp/network/RecipeService.kt
@@ -1,4 +1,31 @@
 package com.example.recipeapp.network
 
+import com.example.recipeapp.models.CategoriesResponse
+import com.example.recipeapp.models.Meal
+import com.example.recipeapp.models.RecipeResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface RecipeService {
+
+    @GET("api/json/v1/1/random.php")
+    suspend fun getRandomMeal(): Meal
+
+    @GET("api/json/v1/1/search.php")
+    suspend fun getMealsByFirstLetter(
+        @Query("f") letter: String
+    ): RecipeResponse
+
+    @GET("api/json/v1/1/search.php")
+    suspend fun searchMealByName(
+        @Query("s") phrase: String
+    ): RecipeResponse
+
+    @GET("api/json/v1/1/categories.php")
+    suspend fun listAllMealCategouris(): CategoriesResponse
+
+    @GET("api/json/v1/1/filter.php?c=Seafood")
+    suspend fun getMealsByCategory(
+        @Query("c") category: String
+    )
 }
