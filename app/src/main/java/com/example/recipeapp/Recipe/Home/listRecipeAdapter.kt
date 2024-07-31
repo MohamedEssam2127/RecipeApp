e605ac097b75a6ae01f5fdd4c9d09bd6684a2965
mo Essam <msam01619@gmail.com>
2024-07-31 23:04:41 +0300

    edit on fav

--- app/src/main/java/com/example/recipeapp/Recipe/Home/listRecipeAdapter.kt
+++ app/src/main/java/com/example/recipeapp/Recipe/Home/listRecipeAdapter.kt
@@ -82,11 +82,11 @@ class listRecipeAdapter(private val recipes: RecipeResponse ,val viewModel: Favo
         holder.iconFav.setOnClickListener {
             val favoriteMeal = FavoriteMeal(
                 idMeal = recipes.meals[position].idMeal.toInt(),
-                strCategory=  recipes.meals[0].strCategory,
-                strMeal= recipes.meals[0].strMeal,
-                strMealThumb = recipes.meals[0].strMealThumb,
-                strTags = recipes.meals[0].strTags,
-                strYoutube= recipes.meals[0].strYoutube,
+                strCategory=  recipes.meals[position].strCategory,
+                strMeal= recipes.meals[position].strMeal,
+                strMealThumb = recipes.meals[position].strMealThumb,
+                strTags = recipes.meals[position].strTags,
+                strYoutube= recipes.meals[position].strYoutube,
                 userId= userId
 
             )
